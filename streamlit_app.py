d84aa29c0d9f42f2221c74655c592de76ea6df5d
Sujan <sujanshahi050@yahoo.com>
2023-12-14 20:50:15 +0000

    Added chart, grids, and date range

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,48 +1,91 @@
 # streamlit_app.py
 
+# Importing Libraries
 import streamlit as st
 import altair as alt
 import pandas as pd
+import plotly.graph_objects as go
 import plotly.express as px
 from st_files_connection import FilesConnection
-
+from datetime import datetime
 
 # Function to get data
 @st.cache_data
 def get_data():
     conn = st.connection('gcs', type=FilesConnection)
     data = conn.read("sif_bucket/sample_data_11_19_23.csv", ttl=600)
-    data['DATE'] = pd.to_datetime(data['DATE'], format = "%Y%m%d")
+    data['DATE'] = pd.to_datetime(data['DATE'], format = '%Y%m%d').dt.strftime('%Y-%m-%d')
     return data
 
+# Assign the data to a dataframe
 df = get_data()
 
-st.title("Website")
+# Select variable for comparison
+selected_variables = st.multiselect('Select Variable', df.columns[2:])
 
-# Dropdown to select company
-selected_company = st.selectbox("Select Company", df['SYMBOL'].unique())
+# Select ticker
+selected_ticker = st.multiselect('Select Ticker', df['SYMBOL'].unique(), default=df['SYMBOL'].unique()[0])
 
-# Filter data for the selected company
-filtered_df = df[df['SYMBOL'] == selected_company]
+# Filter dataframe based on selected ticker(s)
+filtered_df = df[df['SYMBOL'].isin(selected_ticker)]
 
-# Create time series plot
-fig1 = px.line(filtered_df, x='DATE', y='quoted_spread', title=f'{selected_company} Quoted Spread Time Series')
-fig1.update_xaxes(title_text='Date')
-fig1.update_yaxes(title_text='Quoted Spread')
+# Create line chart with range selector button
+fig = px.line(filtered_df, x='DATE', y=selected_variables, color='SYMBOL',
+              labels={'value': 'Value', 'variable': 'Metric'})
 
-fig2 = px.line(filtered_df, x='DATE', y='effective_spread', title=f'{selected_company} Effective Spread')
-fig2.update_xaxes(title_text='Date')
-fig2.update_yaxes(title_text='Effective Spread')
+# Add date range slider within the chart layout
+fig.update_layout(
+    xaxis_rangeslider_visible=True,
+    xaxis_title='Date Range',
+    yaxis_tickformat=".6f",  # Adjust this format based on your preference
+)
 
-fig3 = px.line(filtered_df, x='DATE', y='volatility_trade', title=f'{selected_company} Volatility Trade')
-fig3.update_xaxes(title_text='Date')
-fig3.update_yaxes(title_text='Volatility Trade')
+# Add range selector buttons and show gridlines
+fig.update_layout(
+    xaxis=dict(
+        rangeselector=dict(
+            buttons=list([
+                dict(count=1, label="1d", step="day", stepmode="backward"),
+                dict(count=7, label="1w", step="day", stepmode="backward"),
+                dict(count=1, label="1mo", step="month", stepmode="backward"),
+                dict(count=6, label="6mo", step="month", stepmode="backward"),
+                dict(step="all")
+            ]),
+            bgcolor='green',  # Background color of range selector
+            font=dict(color='white')  # Font color of range selector
+        ),
+        rangeslider=dict(visible=True),
+        type="date",
+        tickfont=dict(color='black'),  # Font color of x-axis ticks
+        showgrid=True,  # Show x-axis gridlines
+        gridcolor='grey'  # Color of gridlines
+    ),
+    yaxis=dict(
+        title=dict(text=", ".join(selected_variables), font=dict(color='white')),  # Join variables into a single string  # Font color of y-axis title
+        tickfont=dict(color='black'),  # Font color of y-axis ticks
+        showgrid=True,  # Show y-axis gridlines
+        gridcolor='grey',  # Color of gridlines
+        rangemode='tozero'  # Start y-axis from zero
+    ),
+    paper_bgcolor='white',  # Background color of the entire chart
+    plot_bgcolor='white'  # Background color of the plot area
+)
+
+
+# Display the chart
+st.plotly_chart(fig)
+# Plot the data
 
 
 
-# Display the plot for quoted spread
-st.plotly_chart(fig1)
-st.plotly_chart(fig2)
-st.plotly_chart(fig3)
+
+
+
+
+
+
+
+
+
 
 
